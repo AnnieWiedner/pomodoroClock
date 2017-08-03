49b360c7feb9137487680e78949762b4fea8bf95
Annie <annie.wiedner@gmail.com>
2017-08-03 09:06:41 -0500

    styling and user settings

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 var currentTime = new Date().getTime();
-var sessionInMin = 1;
-var breakInMin = 2;
+var sessionInMin = 15;
+
+var sessionInSec = 0;
+var breakInMin = 5;
 var endTime = currentTime + (sessionInMin * 60000);
 var breakEnd = currentTime + (breakInMin * 60000);
-var clockMin = document.getElementById('minutes');
-clockMin.innerHTML = ('0' + sessionInMin).slice(-2);
-remainingTime(endTime);
+
+var clockMin1 = document.getElementById('minutes1');
+var clockMin2 = document.getElementById('minutes2');
+var clockSec1 = document.getElementById('seconds1');
+var clockSec2 = document.getElementById('seconds2');
+clockMin1.innerHTML = ('0' + sessionInMin).slice(-2, -1);
+clockMin2.innerHTML = ('0' + sessionInMin).slice(-1);
+clockSec1.innerHTML = ('0' + sessionInSec).slice(-2, -1);
+clockSec2.innerHTML = ('0' + sessionInSec).slice(-1);
+
+var breakNum = document.getElementById('breakNum');
+breakNum.innerHTML = breakInMin;
+
+var sessionNum = document.getElementById('sessionNum');
+sessionNum.innerHTML = sessionInMin;
+
+document.getElementById("start").addEventListener("click", updateSessionClock);
 
 
 
@@ -23,16 +39,19 @@ function remainingTime(end) {
 }
 
 function updateSessionClock() {
-  var clockMin = document.getElementById("minutes");
-  var clockSec = document.getElementById("seconds");
   var timeIntrvlSession = setInterval(function(){
     var t = remainingTime(endTime);
-    clockMin.innerHTML = ('0' + t.minutes).slice(-2);
-    clockSec.innerHTML = ('0' + t.seconds).slice(-2);
+    // putting one digit in each box
+    clockMin1.innerHTML = ('0' + t.minutes).slice(-2, -1);
+    clockMin2.innerHTML = ('0' + t.minutes).slice(-1);
+    clockSec1.innerHTML = ('0' + t.seconds).slice(-2, -1);
+    clockSec2.innerHTML = ('0' + t.seconds).slice(-1);
     if (t.total <= 0) {
       clearInterval(timeIntrvlSession);
-      clockMin.innerHTML = ('0' + breakInMin).slice(-2);
-      clockSec.innerHTML = ('00').slice(-2);
+      clockMin1.innerHTML = ('0' + breakInMin).slice(-2, -1);
+      clockMin2.innerHTML = ('0' + breakInMin).slice(-1);
+      clockSec1.innerHTML = ('00').slice(-2, -1);
+      clockSec2.innerHTML = ('00').slice(-1);
       breakEnd = currentTime + (breakInMin * 60000);
       remainingTime(breakEnd);
       updateBreakClock();
@@ -41,18 +60,19 @@ function updateSessionClock() {
 }
 
 function updateBreakClock() {
-  var clockMin = document.getElementById("minutes");
-  var clockSec = document.getElementById("seconds");
   var timeIntrvlBreak = setInterval(function(){
     var t = remainingTime(breakEnd);
-    clockMin.innerHTML = ('0' + t.minutes).slice(-2);
-    clockSec.innerHTML = ('0' + t.seconds).slice(-2);
+    // putting one digit in each box
+    clockMin1.innerHTML = ('0' + t.minutes).slice(-2, -1);
+    clockMin2.innerHTML = ('0' + t.minutes).slice(-1);
+    clockSec1.innerHTML = ('0' + t.seconds).slice(-2, -1);
+    clockSec2.innerHTML = ('0' + t.seconds).slice(-1);
     if (t.total <= 0) {
       clearInterval(timeIntrvlBreak);
-      clockMin.innerHTML = '00';
-      clockSec.innerHTML = '00';
+      clockMin1.innerHTML = '0';
+      clockMin2.innerHTML = '0';
+      clockSec1.innerHTML = '0';
+      clockSec2.innerHTML = '0';
     }
   }, 1000);
 }
-
-updateSessionClock();
